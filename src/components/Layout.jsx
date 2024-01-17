d6f9ae8114a68ecab80cabb040ff57a200ad455c
no-humans <pranavprasad242@gmail.com>
2024-01-17 11:06:11 +0530

    Add routing

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -7,6 +7,7 @@ const Layout = () => {
   return (
     <>
       <Header />
+      <Outlet/>
       <Footer/>
     </>
   );
--- src/index.js
+++ src/index.js
@@ -1,13 +1,80 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { createBrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./components/Layout";
+import ErrorPage from "./pages/ErrorPage";
+import Home from "./pages/Home";
+import PostDetail from "./pages/PostDetail";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import UserProfile from "./pages/UserProfile";
+import Authors from "./pages/Authors";
+import CreatePosts from "./pages/CreatePosts";
+import EditPosts from "./pages/EditPosts";
+import CategoryPosts from "./pages/CategoryPosts";
+import AuthorPosts from "./pages/AuthorPosts";
+import Dashboard from "./pages/Dashboard";
+import Logout from "./pages/Logout";
 
-const router = createBrowserRouter
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Home /> },
+      {
+        path: "posts/:id",
+        element: <PostDetail />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "profile/:id",
+        element: <UserProfile />,
+      },
+      {
+        path: "authors",
+        element: <Authors />,
+      },
+      {
+        path: "create",
+        element: <CreatePosts />,
+      },
+      {
+        path: "posts/categories/:category",
+        element: <CategoryPosts />,
+      },
+      {
+        path: "posts/users/:id",
+        element: <AuthorPosts />,
+      },
+      {
+        path: "myposts/:id",
+        element: <Dashboard />,
+      },
+      {
+        path: "posts/:id/edit",
+        element: <EditPosts />,
+      },
+      {
+        path: "logout",
+        element: <Logout />,
+      },
+    ],
+  },
+]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <h1>Hello</h1>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
