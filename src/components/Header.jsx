9555285772070eab9006238823514041c04c7170
no-humans <pranavprasad242@gmail.com>
2024-01-18 13:19:06 +0530

    Style the Header,Footer and errorpage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import {FaBars} from 'react-icons/fa'
+import {AiOutlineClose} from "react-icons/ai"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <nav>
+        <div className='container nav__container'>
+            <Link to="/" className='nav__logo'>
+                <img src="https://i.postimg.cc/sDVHM5tF/starbucks-logo-png-1666.png" alt='LOgO'/>
+            </Link>
+            <ul className='nav__menu'>
+                <li><Link to="/profile">Ragnar</Link></li>
+                <li><Link to="/create">Create Post</Link></li>
+                <li><Link to="/author">Authors</Link></li>
+                <li><Link to="/logout">Logout</Link></li>
+            </ul>
+            <button className='nav__toggle-btn'>
+                <AiOutlineClose/>
+            </button>
+        </div>
+    </nav>
   )
 }
 
-export default Header
+export default Header 
