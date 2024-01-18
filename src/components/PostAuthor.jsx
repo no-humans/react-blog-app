b98c1e722d249430ce3d736b4b43e1444ef9db16
no-humans <pranavprasad242@gmail.com>
2024-01-18 14:33:40 +0530

    Created PostAuthor, POstItem,Posts

---
+++ src/components/PostAuthor.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import avatar from '../images/avatar.jpeg'
+
+const PostAuthor = () => {
+  return (
+    <Link tp={`/posts/users/sdfdf`}>
+        <div className="post__author-avatar">
+            <img src={avatar} alt=''/>
+        </div>
+        <div className="post__author-details">
+            <h5>By: Ragnar</h5>
+            <small>Just Now</small>
+        </div>
+    </Link>
+  )
+}
+
+export default PostAuthor
---
+++ src/components/Posts.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import cloudy from "../images/cloudy.jpg";
+import PostItem from "./PostItem";
+
+const DUMMY_POSTS = [
+  {
+    id: "1",
+    thumbnail: cloudy,
+    category: "education",
+    title: "Title of the post",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus illo magnam modi fuga natus voluptate rem saepe, provident cupiditate obcaecati sint sunt nostrum eum reprehenderit necessitatibus quae quod.",
+    authorID: 3,
+  },
+  {
+    id: "2",
+    thumbnail: cloudy,
+    category: "Agriculure",
+    title: "Title of the post",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus illo magnam modi fuga natus voluptate rem saepe, provident cupiditate obcaecati sint sunt nostrum eum reprehenderit necessitatibus quae quod.",
+    authorID: 2,
+  },
+  {
+    id: "3",
+    thumbnail: cloudy,
+    category: "science",
+    title: "Title of the post",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus illo magnam modi fuga natus voluptate rem saepe, provident cupiditate obcaecati sint sunt nostrum eum reprehenderit necessitatibus quae quod.",
+    authorID: 1,
+  },
+  {
+    id: "4",
+    thumbnail: cloudy,
+    category: "weather",
+    title: "Title of the post",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus illo magnam modi fuga natus voluptate rem saepe, provident cupiditate obcaecati sint sunt nostrum eum reprehenderit necessitatibus quae quod.",
+    authorID: 4,
+  },
+];
+
+const Posts = () => {
+  const [posts, setPosts] = useState(DUMMY_POSTS);
+  return (
+    <section className="posts">
+      {posts.map(({id, thumbnail, category, title, desc, authorID}) => (
+        <PostItem
+          key={id}
+          PostID={id}
+          thumbnail={thumbnail}
+          category={category}
+          title={title}
+          description={desc}
+          authorID={authorID}
+        />
+      ))}
+    </section>
+  );
+};
+
+export default Posts;
